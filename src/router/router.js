10bffcb80a55ba41fe9cb16f253ffcd51a03b4b9
Andry-code-web <82222276+Andry-code-web@users.noreply.github.com>
2024-05-14 16:21:30 -0500

    vercion_final

--- src/router/router.js
+++ src/router/router.js
@@ -6,27 +6,30 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'sebastianandryescalantemendoza@gmail.com',
-        pass: 'hkjq vrei adrh pdas'
+        user: 'mendozabruno455@gmail.com',
+        pass: 'oxyb rkhb nkfz duri'
     }
 });
 
 // Función para enviar correo electrónico
 const enviarCorreo = (destinatario, nombre, telefono, mensaje) => {
     const mailOptions = {
-        from: 'sebastianandryescalantemendoza@gmail.com',
+        from: 'mendozabruno455@gmail.com',
         to: destinatario,
         subject: 'Super BIKE',
         html: `
             <p>Bienvenido ${nombre} con numero ${telefono}, ya eres parte de Super BIKE.</p>
             <p>Mensaje: ${mensaje}</p>
-            <p>¡Gracias por unirte a nosotros! Esperamos que disfrutes de nuestros servicios.</p>
+            <p>
+            ¡Gracias por unirte a nosotros! Esperamos que disfrutes de nuestros servicios.</br>
+            <a href="https://superbikeeirl.wixsite.com/inicio/bicicletas">Has click para acceder a la tienda&#9757;</a>
+            </p>
             <img src="cid:oferta" alt="Super BIKE">
         `,
         attachments: [
             {
                 filename: '6.png', // Nombre de la imagen adjunta
-                path: './public/img/6.png', // Ruta de la imagen en tu servidor
+                path: './public/img/ofera_de_bicicleta.png', // Ruta de la imagen en tu servidor
                 cid: 'oferta' // Identificador único para la imagen
             }
         ]
